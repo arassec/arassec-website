9f3b41a460f6503cbee0ac4bcee0d6ad63a23694
Andreas Sensen <andreas.sensen@arassec.com>
2020-06-21 20:53:40 +0200

    Initial commit.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: "arassec.com",
+    themeConfig: {
+        searchMaxSuggestions: 25,
+        nav: [
+            {text: "Home", link: "/"},
+            {text: "Igor", link: "https://www.arassec.com/igor"},
+            {text: "GitHub", link: "https://www.github.com/arassec"}
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+# arassec website
+
+This is my personal website.
+
+# Projects
+
+My current main project is [igor](https://www.arassec.com/igor), a tool for managing 'continuous workarounds'.
+
+# Contact
+
+You can contact me via e-mail under andreas.sensen@arassec.com
